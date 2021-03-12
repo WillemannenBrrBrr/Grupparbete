f65a86e0618f1516a8debbe89e43755d991b8c9a
Emilia <princessekrom@gmail.com>
2021-03-12 13:15:10 +0100

    s

--- include/CApp.php
+++ include/CApp.php
@@ -27,7 +27,13 @@ class CApp
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title><?php echo($title)?></title>
             <link rel="stylesheet" href="style/general.css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" 
+            integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
         </head>
+        <header>
+
+        </header>
+        <section class="container">
         <body>
 
         <?php
@@ -38,6 +44,7 @@ class CApp
         ?>
 
         <script src="script/tools.js"></script>
+        </section>
         </body>
         </html>
 
