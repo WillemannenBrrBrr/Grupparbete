81a1b6fc0d1ff545df6addd983192ec7ebb0f101
WillemannenBrrBrr <william.danielsson@vbu.se>
2021-03-26 09:23:28 +0100

    :)

--- alreadyBooked.php
+++ alreadyBooked.php
@@ -3,7 +3,25 @@ require_once("include/CApp.php");
 
 $app->renderHeader("Home"); 
 
-$app->getForm()->open("alreadyBooked");
+if(!empty($_POST))
+{
+    $name = $_POST["name"];
+    $number = $_POST["number"];
+    $email = $_POST["email"];
+
+    $query = "SELECT `namn`, `nummer`, `email`, `bord` FROM `booking` WHERE namn='$name' AND nummer='$number' AND email='$email'";
+    $result = $app->getdb()->query($query);
+    if($result->num_rows != 0)
+    {
+        echo('')
+    }
+    else
+    {
+        echo('Du har ingen bokning');
+    }
+}
+
+$app->getForm()->open("alreadyBookedForm");
 $app->getForm()->createInput("text", "name", "För/Efternamn");
 $app->getForm()->createInput("email", "email", "E-mail");
 $app->getForm()->createInputTel("number", "Telefonnummer");
