4617bacb40e790b5e1f188ad0e76963f2898ddda
WillemannenBrrBrr <william.danielsson@vbu.se>
2021-04-14 10:53:27 +0200

    tjo

--- selectTable.php
+++ selectTable.php
@@ -14,6 +14,7 @@ if(!empty($_POST))
     $query = "SELECT `available` FROM `tables` WHERE id = $table";
     $result = $app->getdb()->query($query);
     $data = $result->fetch_assoc();
+    
     if($data["available"] == 0)
     {
         echo("bordet är redan bokat");
