c74756fed4fad777b9a156ba58842458e600898b
WillemannenBrrBrr <william.danielsson@vbu.se>
2021-04-23 14:58:27 +0200

    tid och datum formulär fixat, db behöver uppdatera

--- phpMyAdmin/booking.sql
+++ phpMyAdmin/booking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: localhost
--- Tid vid skapande: 24 mars 2021 kl 10:34
+-- Tid vid skapande: 23 apr 2021 kl 14:56
 -- Serverversion: 10.4.12-MariaDB
 -- PHP-version: 7.4.4
 
@@ -29,50 +29,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `booking` (
   `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
   `namn` varchar(40) NOT NULL,
   `nummer` varchar(10) NOT NULL,
   `email` varchar(100) NOT NULL,
   `bord` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumpning av Data i tabell `booking`
---
-
-INSERT INTO `booking` (`id`, `namn`, `nummer`, `email`, `bord`) VALUES
-(1, 'William Danielsson', '0730860327', 'william.danielsson@vbu.se', 4);
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur `tables`
---
-
-CREATE TABLE `tables` (
-  `id` int(11) NOT NULL,
-  `available` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumpning av Data i tabell `tables`
---
-
-INSERT INTO `tables` (`id`, `available`) VALUES
-(1, 1),
-(2, 1),
-(3, 1),
-(4, 0),
-(5, 1),
-(6, 1),
-(7, 1),
-(8, 1),
-(9, 1),
-(10, 1),
-(11, 1),
-(12, 1),
-(13, 1),
-(14, 1),
-(15, 1);
 
 --
 -- Index för dumpade tabeller
@@ -85,12 +48,6 @@ ALTER TABLE `booking`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index för tabell `tables`
---
-ALTER TABLE `tables`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT för dumpade tabeller
 --
 
@@ -98,13 +55,7 @@ ALTER TABLE `tables`
 -- AUTO_INCREMENT för tabell `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT för tabell `tables`
---
-ALTER TABLE `tables`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
