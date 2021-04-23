5bbaba98e47b2e6746ccfe357aa952d6056cb746
Emilia <princessekrom@gmail.com>
2021-04-23 12:01:43 +0200

    d

--- include/CApp.php
+++ include/CApp.php
@@ -30,7 +30,7 @@ class CApp
             <link rel="stylesheet" href="style/general.css">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" 
             integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-            <link rel="icon" href="img/favicon.ico" />
+            <link rel="icon" href="img/favicon.ico" alt="favicon"/>
         </head>
         <body>
         <header>
