ac7d49823f670b87533707106e659ea38bb606ff
Emilia <princessekrom@gmail.com>
2021-04-23 13:43:08 +0200

    Ändringar i CSS

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ require_once("include/CApp.php");
 
 $app->renderHeader("Kontakt"); 
 ?>
-    <h3>Kontaktuppgifter</h3>
+    <h2>Kontaktuppgifter</h2>
     <p>Bosses Restaurang - Nynäshamn</p>
     <p>096 36 Stenmarksvägen 13</p>
     <p>0240-34721</p>
