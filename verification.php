50704d9497fd9bafa7ef7c3a75fa6909c750b7fd
WillemannenBrrBrr <william.danielsson@vbu.se>
2021-05-26 08:27:42 +0200

    fixat en varifications sida, inte klar men på börjad

--- verification.php
+++ verification.php
@@ -3,11 +3,11 @@ require_once("include/CApp.php");
 
 $app->renderHeader("Tack");
 $table = $_GET["table"];
-$time = date("d-m-Y H:i", $_GET["time"]);
+$time = $_GET["time"];
 
 ?>
 <h2>Tack för din beställning</h2>
-<p>Du har bokat bord <?php echo($table) ?> till den <?php echo($time) ?></p>
+<p>Du har bokat bord <?php echo($table) ?> till den <?php echo(date("d-m-Y H:i", $time)) ?></p>
 <?php
 
 $app->renderFooter(); 
