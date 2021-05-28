7498403be370ea688ee88a1c339327388982361a
WillemannenBrrBrr <william.danielsson@vbu.se>
2021-05-28 08:19:55 +0200

    kom ihåg att byta filnamn till "grupparbete"

--- restaurantMenu.php
+++ restaurantMenu.php
@@ -6,8 +6,8 @@ $app->renderHeader("Meny");
 ?>
 
 <div class="sideBySide">
-    <img src="../Grupparbete-2/img/menu1.jpg" alt="Meny bild 1" class="responsiveMenu">
-    <img src="../Grupparbete-2/img/menu2.jpg" alt="Meny bild 2" class="responsiveMenu">
+    <img src="../Grupparbete/img/menu1.jpg" alt="Meny bild 1" class="responsiveMenu">
+    <img src="../Grupparbete/img/menu2.jpg" alt="Meny bild 2" class="responsiveMenu">
 </div>
 
 <?php
